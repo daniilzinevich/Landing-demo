d96a82580e79096504edd753108837a77881e5c4
Daniil Zinevich <danila2412@gmail.com>
2019-03-21 15:29:03 +0300

    implement landing in static

---
+++ static/app/index.js
@@ -0,0 +1,7 @@
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+import './base.css'
+import 'uikit/dist/css/uikit.min.css';
+
+// loads the Icon plugin
+UIkit.use(Icons);
